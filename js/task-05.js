5e44c0c7f665d4d532b5f33eb2601bb120bde943
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-05-19 08:06:11 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,9 @@ const textOutput = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
     if (event.currentTarget.value !== "") {
         textOutput.textContent = event.currentTarget.value;       
-    };
-    if (event.currentTarget.value == "") {
-        textOutput.textContent = "Anonymous";       
     }
+    else {
+        textOutput.textContent = "Anonymous"
+    };
 });
 
